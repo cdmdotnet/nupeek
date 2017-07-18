2f3af22e7cba03a7166d2091fb41ce1816c915e1
Grover Jackson <grover@cdmdotnet.com>
2017-07-18 23:59:19 +1200

    Fixed sources not working... they now work.

--- NuPeek/DataServices/ISymbolPackagePathResolver.cs
+++ NuPeek/DataServices/ISymbolPackagePathResolver.cs
@@ -3,5 +3,7 @@ namespace NuPeek.DataServices
 	public interface ISymbolPackagePathResolver
 	{
 		string GetSymbolPackagePath(string packageId, string version);
+
+		string GetSymbolSourcePath(string packageId, string version);
 	}
 }
